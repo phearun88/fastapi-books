076972a12d4572c9d6613399ed0e1d5a36e651e7
phearun168 <phin.phearun2013@gmail.com>
2026-03-16 14:33:15 -0400

    fix get data

--- app/models/book_models.py
+++ app/models/book_models.py
@@ -6,14 +6,11 @@ class BookBase(BaseModel):
     author: str = Field(..., min_length=1, max_length=100)
     category: str = Field(..., min_length=1, max_length=50)
 
-
 class BookCreate(BookBase):
     pass
 
-
 class BookUpdate(BookBase):
     id: int
-
 
 class BookResponse(BookBase):
     id: int
